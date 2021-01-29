c2e93cebe749ba4e287d187bf209c803294ec272
Daniel <dmadera0@gmail.com>
2021-01-28 19:29:11 -0700

    fetched character

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//state compent class componnet//
+class App extends Component{
+
+  state = {
+    allCharacters:[],
+    selectedCharacters:[],
+  }
+
+componentDidMount(){
+  fetch('https://rickandmortyapi.com/api/character')
+    .then(response =>response.json())
+    .then(data => {
+      this.setState({allCharacters: data.results})
+      this.setState({selectedCharacters: data.results})
+
+    })
+}
+  render(){
+    return (
+      <div id="app">
+        <h1> Im motha fukin Rick yall!!!! </h1>
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
